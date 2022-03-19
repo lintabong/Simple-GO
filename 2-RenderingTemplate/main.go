04c8d4f8da3dc171474525f696f4f100251249bc
Lintang Bongkar Girana <96236126+lintabong@users.noreply.github.com>
2022-03-19 05:32:30 -0700

    Create main.go

---
+++ 2-RenderingTemplate/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"path"
+	"text/template"
+)
+
+func index(w http.ResponseWriter, f *http.Request){
+	var filepath = path.Join("views","index.html")
+	var tmpl, err = template.ParseFiles(filepath)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		return
+	}
+
+	var data = map[string]interface{}{
+		"title" : "Main Page",
+		"name"	: "lintang",
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", index)
+
+	fmt.Println("Server started at localhost:8090")
+	http.ListenAndServe(":8090", nil)
+}
